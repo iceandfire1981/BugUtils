479407396a755e3cadef200bfcc911da8c8d4fe1
AlxSlash <androidtraces@gmail.com>
2016-12-14 00:41:58 +0800

    Add somethings

---
+++ src/main/java/com/tools/BugUtils/ConfigFileUtils.java
@@ -0,0 +1,10 @@
+package com.tools.BugUtils;
+
+public final class ConfigFileUtils {
+    private static final String TAG_CONFIG_ROOT = "bug_config";
+    private static final String TAG_CONFIG_GET_TIMES = "get_times";
+    private static final String TAG_CONFIG_LIST = "config_list";
+    private static final String TAG_CONFIG = "config";
+    private static final String TAG_CONFIG_COS = "comp_list";
+    private static final String TAG_CONFIG_FILE_PRE = "file_name_pre";
+}
