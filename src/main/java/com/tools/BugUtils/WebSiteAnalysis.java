19bc166dfec17af35d031994854c3264c29edcd1
iceandfire2016 <iceandfire2016@hotmail.com>
2016-12-26 14:18:21 +0800

    Modify somethings

---
+++ src/main/java/com/tools/BugUtils/WebSiteAnalysis.java
@@ -0,0 +1,12 @@
+package com.tools.BugUtils;
+
+public class WebSiteAnalysis {
+
+    private String mBaseAddress;
+    
+    public WebSiteAnalysis(String base_address) {
+        // TODO Auto-generated constructor stub
+        mBaseAddress = base_address;
+    }
+
+}
